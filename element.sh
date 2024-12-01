bcf13c989bf9b170cae4cf3720d1892cd22b252c
Kristin Wiseman <kristin.w.dev@gmail.com>
2024-12-01 23:13:02 +0000

    feat: first attempt at invalid handling.

--- element.sh
+++ element.sh
@@ -7,8 +7,30 @@ if [[ -z $1 ]]; then
 # Check if it's an atomic number
 elif [[ $1 =~ ^[0-9]+$ ]]; then
   QUERY_RESULT=$($PSQL "SELECT * FROM elements WHERE atomic_number = $1;")
-  echo "$QUERY_RESULT";
+  
+  if [[ -z $QUERY_RESULT ]]; then
+    echo -e "\nI could not find that element in the database."
+  else
+    # query properties table
+    echo ""
+  fi
 else
-  # test for symbol or name
-  echo ""
+  # test for symbol
+  QUERY_RESULT=$($PSQL "SELECT * FROM elements WHERE symbol = '$1';")
+  
+  # if no results with symbol, check for name
+  if [[ -z $QUERY_RESULT ]]; then
+    QUERY_RESULT=$($PSQL "SELECT * FROM elements WHERE name = '$1';")
+    
+    # if still empty, it doesn't exist
+    if [[ -z $QUERY_RESULT ]]; then
+      echo -e "\nI could not find that element in the database."
+    else
+      # query properties table w/ results from name.
+      echo ""
+    fi
+  else
+    # query properties table with results from symbol.
+    echo ""
+  fi
 fi
