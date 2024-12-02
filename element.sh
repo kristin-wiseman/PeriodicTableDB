9959167c5f5e45e48ccd4039ca633789c2caa1d8
Kristin Wiseman <kristin.w.dev@gmail.com>
2024-12-02 00:44:14 +0000

    feat: print successful query results.

--- element.sh
+++ element.sh
@@ -4,33 +4,28 @@ PSQL="psql -X --username=freecodecamp --dbname=periodic_table -t -c"
 # if no argument provided
 if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
-# Check if it's an atomic number
-elif [[ $1 =~ ^[0-9]+$ ]]; then
-  QUERY_RESULT=$($PSQL "SELECT * FROM elements WHERE atomic_number = $1;")
-  
+else
+  if [[ $1 =~ ^[0-9]+$ ]]; then
+    # if input is numerical, query atomic number column
+    COL="atomic_number"
+  elif [[ ${#1} -le 2 ]]; then
+    # if length <= 2, it's a symbol
+    COL="symbol"
+  else
+    # query name column by default
+    COL="name"
+  fi
+
+  QUERY_RESULT=$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) LEFT JOIN types USING(type_id) WHERE $COL = '$1';")
+
+  # if no result
   if [[ -z $QUERY_RESULT ]]; then
     echo "I could not find that element in the database."
   else
-    # query properties table
-    echo ""
-  fi
-else
-  # test for symbol
-  QUERY_RESULT=$($PSQL "SELECT * FROM elements WHERE symbol = '$1';")
-  
-  # if no results with symbol, check for name
-  if [[ -z $QUERY_RESULT ]]; then
-    QUERY_RESULT=$($PSQL "SELECT * FROM elements WHERE name = '$1';")
-    
-    # if still empty, it doesn't exist
-    if [[ -z $QUERY_RESULT ]]; then
-      echo "I could not find that element in the database."
-    else
-      # query properties table w/ results from name.
-      echo ""
-    fi
-  else
-    # query properties table with results from symbol.
-    echo ""
+    # break up $QUERY_RESULT, print output.
+    echo "$QUERY_RESULT" | while read TID BAR ANUM BAR SYMBOL BAR NAME BAR AMASS BAR MP BAR BP BAR TYPE
+    do
+      echo "The element with atomic number $ANUM is $NAME ($SYMBOL). It's a $TYPE, with a mass of $AMASS amu. $NAME has a melting point of $MP celsius and a boiling point of $BP celsius."
+    done
   fi
 fi
